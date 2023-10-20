b0e2becebaa82853611eabdeb1d4261932be4d9c
Corey Urbanke <cju8676@rit.edu>
2023-10-19 20:15:01 -0400

    search component

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,28 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import Song from './Song';
 import User from './User';
 import SimilarSong from './SimilarSong';
+import Search from './Search';
 
 function App() {
   return (
-    <div className="info-page">
-      <div className="info">
-        <Song />
-      </div>
-      <div className="lists">
-        {/* Right side: Two Lists */}
-        <div className="list">
-          <h3>Liked By</h3>
-          <User />
-          <User />
-          <User />
+    <div>
+      <Search />
+      <div className="info-page">
+        <div className="info">
+          <Song />
         </div>
-        <div className="list">
-          <h3>Similar Songs</h3>
-          <SimilarSong />
-          <SimilarSong />
-          <SimilarSong />
+        <div className="lists">
+          {/* Right side: Two Lists */}
+          <div className="list">
+            <h3>Liked By</h3>
+            <User />
+            <User />
+            <User />
+          </div>
+          <div className="list">
+            <h3>Similar Songs</h3>
+            <SimilarSong />
+            <SimilarSong />
+            <SimilarSong />
+          </div>
         </div>
       </div>
     </div>
